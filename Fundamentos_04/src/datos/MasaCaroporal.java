b8d4a0391a778be7d7d934d6dd884bcb32e1fce4
Fran Cenit <fraan.cenit@outlook.es>
2015-10-07 11:42:51 +0200

    prueba github

---
+++ Fundamentos_04/src/datos/MasaCaroporal.java
@@ -0,0 +1,29 @@
+package datos;
+
+import java.util.Scanner;
+
+public class MasaCaroporal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Indique su peso");
+		double peso = sc.nextDouble();
+		
+		
+		System.out.println("Indique su altura");
+		double altura = sc.nextDouble();
+		
+		double imc = sc.nextDouble();
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
